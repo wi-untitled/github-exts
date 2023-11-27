f04e900a8c95d3ff2c9cf0644b52b55b9bd60cdd
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2023-11-27 21:03:59 +0300

    feat: toggle extension iframe

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,5 +1,6 @@
 function init() {
     const iframe = document.createElement("iframe");
+<<<<<<< HEAD
 
     // Styles
     iframe.style.display = "block";
@@ -10,9 +11,22 @@ function init() {
     iframe.style.height = "100%";
     iframe.style.border = "none";
 
+=======
+    iframe.classList.add("github-exts-frame", "github-exts-frame--hidden");
+>>>>>>> e706dc9 (feat: toggle extension iframe)
     iframe.src = chrome.runtime.getURL("contentScript.html");
 
+    const button = document.createElement("button");
+    button.classList.add("github-exts-toggler");
+    button.textContent = "Toggle GitHub Exts";
+
+    function handleToggle() {
+        iframe.classList.toggle("github-exts-frame--hidden");
+    }
+    button.addEventListener("click", handleToggle);
+
     document.body.appendChild(iframe);
+    document.body.appendChild(button);
 }
 
 init();
