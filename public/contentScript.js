e9f52c2edbbe4dc5b976296a62fd80ad1307884c
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2023-11-28 21:51:17 +0300

    fix: closes extentions when user clicks outside
    fix #11

--- public/contentScript.js
+++ public/contentScript.js
@@ -13,15 +13,36 @@ function init() {
     iframe.classList.add("github-exts-frame", "github-exts-frame--hidden");
     iframe.src = chrome.runtime.getURL("contentScript.html");
 
+    /**
+     * Defining button to open and close extentions
+     */
     const button = document.createElement("button");
+
     button.classList.add("github-exts-toggler");
     button.textContent = "Toggle GitHub Exts";
 
     function handleToggle() {
         iframe.classList.toggle("github-exts-frame--hidden");
+        
+        document.body.addEventListener("click", handleClickOutside);
     }
+
+    function handleClickOutside(event) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Event/composedPath
+        const isClickInside = event.composedPath().includes(button);
+
+        if (!isClickInside) {
+            iframe.classList.toggle("github-exts-frame--hidden");
+
+            document.body.removeEventListener("click", handleClickOutside);
+        }
+    }
+
     button.addEventListener("click", handleToggle);
 
+    /** 
+     * Appending elements into body
+     */
     document.body.appendChild(iframe);
     document.body.appendChild(button);
 }
