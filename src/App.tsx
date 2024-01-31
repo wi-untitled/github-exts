100e28ea3052e7d7cf51bc86487d0be6c61a3d65
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-01-31 12:04:10 +0300

    refactor: moves routes into specific files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
 import { observer } from "mobx-react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { LoginScreen, MainScreen, SettingsScreen } from "src/screens";
-import { ProtectedRoute } from "./screens/components";
+import { RouterProvider } from "react-router-dom";
 import { Assets } from "src/components";
+import { router } from "src/routes";
 
 import "./App.css";
-import { InstructionScreen } from "./screens/Instruction";
-
-const router = createBrowserRouter(
-    [
-        {
-            path: MainScreen.routeName,
-            element: <ProtectedRoute element={<MainScreen />} />,
-        },
-        {
-            path: LoginScreen.routeName,
-            element: <LoginScreen />,
-        },
-        {
-            path: InstructionScreen.routeName,
-            element: <InstructionScreen />,
-        },
-        {
-            path: SettingsScreen.routeName,
-            element: <ProtectedRoute element={<SettingsScreen />} />,
-        },
-    ],
-    {
-        basename: "/contentScript.html",
-    },
-);
 
 function AppComponent() {
     return (
