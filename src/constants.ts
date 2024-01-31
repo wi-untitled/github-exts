86fc218073137097beaace6e0b785df175dbb3a4
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-01-31 12:06:35 +0300

    refactor: removes useless variables from files

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,6 @@
-export const HOST = "http://localhost:3000"; // TODO: need to be stored inside [.env]
-
 export enum STORAGE_KEYS {
     CODE = "github-exts/CODE",
     ACCESS_TOKEN = "github-exts/ACCESS_TOKEN",
     TOKEN_TYPE = "github-exts/TOKEN_TYPE",
     SETTINGS = "github-exts/SETTINGS",
 }
-
-export const CLIENT_ID = "Iv1.1247dc257ee98cdd";
-export const REDIRECT_URL =
-    "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID;
