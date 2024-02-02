884647e112debd59a6c76ba83105e109ce6e50d1
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-02-02 23:10:02 +0300

    feat: adds setry integration

--- src/popup/mainPopup.tsx
+++ src/popup/mainPopup.tsx
@@ -6,10 +6,38 @@ import createGlobalServices from "src/services/utils/createGlobalServices.ts";
 import createGlobalStores from "src/stores/utils/createGlobalStores.ts";
 import { getTransport } from "src/transport/Transport.ts";
 import { WelcomeLoading } from "src/components/index.ts";
+import * as Sentry from "@sentry/react";
+import { Dedupe, Debug, HttpClient } from "@sentry/integrations";
 
 import "src/i18n/config";
 
 import "./indexPopup.css";
+
+Sentry.init({
+    release: import.meta.env.APP_VERSION,
+    environment: import.meta.env.MODE,
+    dsn: "https://e6f522d37c7ed1e1ff70895d774a5af8@o422463.ingest.sentry.io/4506678795960320",
+    integrations: [
+        new Sentry.BrowserTracing({
+            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+            tracePropagationTargets: ["localhost", "*"],
+        }),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+        new Dedupe(),
+        new Debug(),
+        new HttpClient(),
+        new Sentry.BrowserProfilingIntegration(),
+        new Sentry.BrowserTracing(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 export async function initialize(): Promise<any> {
     const transport = getTransport();
