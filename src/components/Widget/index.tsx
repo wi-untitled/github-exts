cc1b306015a2792ae9b64f1c7fb48878e86e1084
Maksim Sharov <maxsharoff@gmail.com>
2024-02-10 12:36:06 +0100

    refactor: update tooltip component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren } from "react";
 import { Tooltip } from "react-tooltip";
 import clsx from "clsx";
 import Spinner from "src/components/Spinner";
@@ -7,7 +7,7 @@ import { FeatureFlag } from "src/core";
 interface IWidgetProps {
     isLoading?: boolean;
     headerRight?: React.ReactNode;
-    title: string;
+    title: React.ReactNode;
     info?: string;
     id?: string;
     className?: string;
@@ -18,8 +18,6 @@ export const Widget: FC<PropsWithChildren<IWidgetProps>> = ({
     isLoading = false,
     headerRight,
     title,
-    info,
-    id,
     className,
 }) => {
     return (
@@ -29,28 +27,17 @@ export const Widget: FC<PropsWithChildren<IWidgetProps>> = ({
                 className,
             )}
         >
-            <header
-                data-tooltip-id={id}
-                data-tooltip-content={info}
-                className="bg-light dark:bg-dark px-3 py-2 flex space-x-0 justify-between border-b border-light dark:border-dark"
-            >
-                <strong className="text-sm">{title}</strong>
+            <header className="bg-light dark:bg-dark px-3 py-2 flex space-x-0 justify-between border-b border-light dark:border-dark">
+                {typeof title === "string" ? (
+                    <strong className="text-sm">{title}</strong>
+                ) : (
+                    title
+                )}
                 {headerRight}
             </header>
             <div className="relative bg-white dark:bg-[#0d1117]">
                 {isLoading ? <Spinner /> : children}
             </div>
-            <FeatureFlag name="enableWidgetTitleTooltip">
-                <>
-                    {info && (
-                        <Tooltip
-                            delayShow={3000}
-                            className="!bg-gray-600 !w-[310px]"
-                            id={id}
-                        />
-                    )}
-                </>
-            </FeatureFlag>
         </div>
     );
 };
