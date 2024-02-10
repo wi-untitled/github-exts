b6376d598a424ffd97d90d7dad44b8d18386fa27
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-02-10 20:39:16 +0300

    fix: finishes release flow

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -34,14 +34,7 @@ export class AppStore extends BaseStore implements IState {
         notificationsService: NotificationsService,
     ) {
         super(transport, notificationsService);
-        console.log(42);
-        console.log(42);
-        console.log(42);
-        console.log(42);
-        console.log(42);
-        console.log(42);
-        console.log(42);
-        console.log(42);
+
         makeAutoObservable<
             AppStore,
             "updateIsOpen" | "updateLoading" | "isLoading"
